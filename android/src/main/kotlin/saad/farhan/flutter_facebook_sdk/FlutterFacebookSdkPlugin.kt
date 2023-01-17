a8ea3b1c7c7573adce6b14cbe789ddcd7f043bdf
Andrey Ilyichev <gidroskolbasovich@gmail.com>
2023-01-17 17:25:30 +0300

    test

--- android/src/main/kotlin/saad/farhan/flutter_facebook_sdk/FlutterFacebookSdkPlugin.kt
+++ android/src/main/kotlin/saad/farhan/flutter_facebook_sdk/FlutterFacebookSdkPlugin.kt
@@ -195,20 +195,21 @@ class FlutterFacebookSdkPlugin : FlutterPlugin, MethodCallHandler, StreamHandler
        
         logger = AppEventsLogger.newLogger(context)
 
-        //val targetUri = AppLinks.getTargetUrlFromInboundIntent(context, activityPluginBinding!!.activity.intent)
-      
-        AppLinkData.fetchDeferredAppLinkData(context, 
-  new AppLinkData.CompletionHandler() {
-     @Override
-     public void onDeferredAppLinkDataFetched(AppLinkData appLinkData) {
-         // Process app link data
-          if (appLinkData == null) {
+        val targetUri = AppLinks.getTargetUrlFromInboundIntent(context, activityPluginBinding!!.activity.intent)
+        AppLinkData.fetchDeferredAppLinkData(context, object : AppLinkData.CompletionHandler {
+            override fun onDeferredAppLinkDataFetched(appLinkData: AppLinkData?) {
+
+                if (appLinkData == null) {
                     return;
                 }
-                Log.d("tag1", appLinkData)
+Log.d("tag1", appLinkData)
+                deepLinkUrl = appLinkData.targetUri.toString();
+                if (eventSink != null && deepLinkUrl != null) {
+                    eventSink!!.success(deepLinkUrl)
+                }
+            }
 
-     }
- }
+        })
     }
 
     private fun createBundleFromMap(parameterMap: Map<String, Any>?): Bundle? {
